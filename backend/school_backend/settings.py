fb90f2f327542ccc4aa996effc0ace8961233008
Tarun-428 <tarunpatidar428@gmail.com>
2026-04-22 03:38:43 +0530

    new data

--- backend/school_backend/settings.py
+++ backend/school_backend/settings.py
@@ -207,7 +207,9 @@ CORS_ALLOW_METHODS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
-    "http://3.108.196.134/5173"
+    "http://3.108.196.134:5173",
+    "http://3.108.196.134:5174",
+
 ]
 
 # ─────────────────────────────────────────
